c6b6acdadf3c441780924a02de360adc8b2f9d73
vadym <vhryshchenko@binariks.com>
2022-12-05 09:04:54 +0200

    added ag-grid

---
+++ src/components/UsersListModal/UsersListModal.cy.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+import AGContext from '../../lib/context'
+import UsersListModal from './index'
+
+
+
+it('UsersListModal', () => {
+  cy.mount(<UsersListModal />)
+})
